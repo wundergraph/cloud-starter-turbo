88dab7f2930a950d77c33ebfea3b2a4590821c56
starptech <deusdustin@gmail.com>
2022-12-29 19:59:16 +0100

    chore: upgrade sdk

--- apps/api/wundergraph.server.ts
+++ apps/api/wundergraph.server.ts
@@ -1,5 +1,4 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
-import { configureWunderGraphServer } from '@wundergraph/sdk';
+import { configureWunderGraphServer } from '@wundergraph/sdk/server';
 import type { HooksConfig } from './generated/wundergraph.hooks';
 import type { InternalClient } from './generated/wundergraph.internal.client';
 
@@ -7,24 +6,5 @@ export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
   hooks: {
     queries: {},
     mutations: {}
-  },
-  graphqlServers: [
-    {
-      serverName: 'gql',
-      apiNamespace: 'gql',
-      schema: new GraphQLSchema({
-        query: new GraphQLObjectType({
-          name: 'RootQueryType',
-          fields: {
-            hello: {
-              type: GraphQLString,
-              resolve() {
-                return 'world';
-              }
-            }
-          }
-        })
-      })
-    }
-  ]
+  }
 }));
